3cc5b51aadb9b0852319e9dbcef028892350d8fc
dkarasiewicz <hello@dawid.run>
2026-01-24 01:30:42 +0100

    LAU-27: remove console.log

--- apps/core-e2e/src/support/db.module.mock.ts
+++ apps/core-e2e/src/support/db.module.mock.ts
@@ -9,13 +9,6 @@ import { PoolConfig } from 'pg';
     {
       provide: DB_CONNECTION,
       useFactory: () => {
-        console.log('connecting to db with', {
-          user: process.env,
-          password: process.env['DB_PASS'],
-          database: process.env['DB_DATABASE'],
-          host: process.env['DB_HOST'],
-          port: parseInt(process.env['DB_PORT'], 10),
-        });
         return drizzle({
           connection: {
             user: process.env['DB_USERNAME'] as string,
