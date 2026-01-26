b8e28062b3be10013ccdcae61492fe929701ff1f
dkarasiewicz <dawid@neighbors.co>
2026-01-26 20:59:24 +0100

    fix: move instrumentation-client.ts to /src

--- apps/customer-ui/src/instrumentation-client.ts
+++ apps/customer-ui/src/instrumentation-client.ts
@@ -3,4 +3,5 @@ import posthog from 'posthog-js';
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY as string, {
   capture_pageview: 'history_change',
   person_profiles: 'identified_only',
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
 });
